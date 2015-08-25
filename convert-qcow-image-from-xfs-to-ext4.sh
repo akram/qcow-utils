8379ad5bf8dd7d90fd79c74912c37558ba5fc39f
Akram Ben Aissi <akram@users.noreply.github.com>
2015-08-25 16:27:35 +0200

    Update convert-qcow-image-from-xfs-to-ext4.sh

--- convert-qcow-image-from-xfs-to-ext4.sh
+++ convert-qcow-image-from-xfs-to-ext4.sh
@@ -1,70 +1,100 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
 
-set -e
+readonly xfs_device=/dev/nbd0
+readonly ext4_device=/dev/nbd1
 
-echo "Loading nbd module to enable Network Block-Devices mount for qemu"
+function log {
+    tput bold
+    printf "$1\n"
+    tput sgr0
+}
+
+if [[ $# != 1 ]]; then
+    printf "Usage: $(basename $0) <rhel7.qcow2>\n" >&2
+    exit 1
+fi
+
+readonly xfs_image="$1"
+readonly ext4_image="${xfs_image%.qcow2}-ext4.qcow2"
+readonly wip_ext4_image="$(dirname $ext4_image)/WIP-$(basename $ext4_image)"
+
+log "Checking prerequisites..."
+if [[ ! -f "$xfs_image" ]]; then
+    printf -- "$xfs_image file doesn’t exist.\n" >&2
+    exit 1
+fi
+
+if qemu-img info --output=json "$xfs_image" | jq -e '.format != "qcow2"'; then
+    printf -- "$xfs_image file isn’t a QCow2 image.\n" >&2
+    exit 1
+fi
+
+log "Loading nbd module..."
 modprobe nbd max_part=63
 
-echo "Initializing variables"
-XFS_MOUNT=$(mktemp -d /tmp/xfs-XXXX)
-EXT4_MOUNT=$(mktemp -d /tmp/ext4-XXXX)
+log "Creating mount points..."
+readonly  xfs_mount=$(mktemp -d)
+readonly ext4_mount=$(mktemp -d)
 
-XFS_IMAGE=rhel-guest-image-7.1-20150224.0-puppet.x86_64.qcow2
-EXT4_IMAGE=$(echo $XFS_IMAGE | sed s/qcow2/ext4\.qcow2/ )
+function cleanup {
+    log "Cleanup..."
+    set +e
+    umount -R "$xfs_mount" "$ext4_mount"
+    rmdir "$xfs_mount" "$ext4_mount"
+    qemu-nbd -d "$xfs_device"
+    qemu-nbd -d "$ext4_device"
+    rm -f "$wip_ext4_image"
+    set -e
+}
+trap cleanup EXIT
 
-echo "Source image: " $XFS_IMAGE
-echo "Destination image (ext4):" $EXT4_IMAGE
+partition_id=p1
+xfs_partition=$xfs_device$partition_id
+ext4_partition=$ext4_device$partition_id
 
-XFS_DEVICE=/dev/nbd0
-EXT4_DEVICE=/dev/nbd1
+image_size=$(qemu-img info --output=json "$xfs_image" | jq '."virtual-size"')
 
-qemu-nbd -d $XFS_DEVICE
-qemu-nbd -d $EXT4_DEVICE
-
-PARTITION_ID=p1
-XFS_PARTITION=$XFS_DEVICE$PARTITION_ID
-EXT4_PARTITION=$EXT4_DEVICE$PARTITION_ID
-
-IMAGE_SIZE=$(qemu-img info $XFS_IMAGE |grep "virtual size"|cut -f 4 -d " "|cut -f 2 -d "(" )
-
-qemu-nbd -c $XFS_DEVICE $XFS_IMAGE
-echo "Source image will be accessible under parition $XFS_PARTITION"
-mount $XFS_PARTITION $XFS_MOUNT
-echo "Source image mounted under $XFS_MOUNT"
-
-
-qemu-img create -f qcow2 $EXT4_IMAGE $IMAGE_SIZE
-qemu-nbd -c $EXT4_DEVICE $EXT4_IMAGE
-echo "Destination image $EXT4_IMAGE created with a size of $IMAGE_SIZE bytes"
-
-echo "Creation of $EXT4_PARTITION. Please wait..."
-parted -s -a optimal $EXT4_DEVICE mklabel gpt -- mkpart primary ext4 1 -1
-
-echo "Making ext4 filesystem under $EXT4_PARTITION. Please wait..."
-mkfs.ext4 $EXT4_PARTITION
-echo "Ext4 filesystem created successfully. Partition is mounted under $EXT4_MOUNT"
-mount $EXT4_PARTITION $EXT4_MOUNT
-
-
-echo "Copying source files to destination images. Please wait..."
-cp -a $XFS_MOUNT/* $EXT4_MOUNT
-
-echo "Files copy completed"
-
-XFS_UUID=$(blkid  -s UUID  $XFS_PARTITION |cut -f2 -d\")
-EXT4_UUID=$(blkid  -s UUID  $EXT4_PARTITION |cut -f2 -d\")
-
-
-echo "Replacing filesystem UUID in configuration files"
-for file in /etc/fstab /boot/grub/grub.conf /boot/grub2/grub.cfg; do
-	sed -i s/$XFS_UUID/$EXT4_UUID/g   $EXT4_MOUNT/$file 
+log "Mounting the xfs-formated partition..."
+qemu-nbd -r -c "$xfs_device" "$xfs_image"
+while [[ ! -e "$xfs_partition" ]]; do
+    sleep .1
 done
-echo "UUID replacements done"
-echo "Disabling SELinux"
-sed -i s/SELINUX=enforcing/SELINUX=disabled/g   $EXT4_MOUNT/etc/selinux/config
+mount -o ro "$xfs_partition" "$xfs_mount"
 
+log "Creating the ext4-formated disk..."
+qemu-img create -f qcow2 "$wip_ext4_image" "$image_size"
+qemu-nbd -c "$ext4_device" "$wip_ext4_image"
 
+log "Formating the ext4-formated disk..."
+parted -s "$ext4_device" -- mklabel msdos \
+                            mkpart primary ext4 1 -1 \
+                            set 1 boot on
+mkfs.ext4 "$ext4_partition"
 
+log "Mounting the ext4-formated partition..."
+mount "$ext4_partition" "$ext4_mount"
 
+log "Copying source files to destination images..."
+cp -a "$xfs_mount"/* "$ext4_mount"
 
+log "Patching files where partition UUID are used"
+readonly  xfs_uuid=$(blkid -o value -s UUID "$xfs_partition")
+readonly ext4_uuid=$(blkid -o value -s UUID "$ext4_partition")
 
+for file in /etc/fstab \
+            /boot/grub/grub.conf \
+            /boot/grub2/grub.cfg; do
+	sed -i "s/$xfs_uuid/$ext4_uuid/g" "$ext4_mount/$file"
+done
+
+log "Disabling SELinux..."
+sed -i "s/^SELINUX=.*/SELINUX=disabled/g" "$ext4_mount/etc/selinux/config"
+
+log "Installing GRUB"
+for m in /dev /proc /sys /dev/pts; do
+    mount --bind "$m" "$ext4_mount$m"
+done
+chroot "$ext4_mount" /usr/sbin/grub2-install --recheck --target=i386-pc "$ext4_device"
+
+mv "$wip_ext4_image" "$ext4_image"
